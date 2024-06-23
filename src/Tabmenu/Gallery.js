bb8bfbfdec83168bb24b763452c2e9da0b00c71b
Lamia Fatiha <f6lamia@gmail.com>
2024-06-23 22:34:29 +0600

    installation done

---
+++ src/Tabmenu/Gallery.js
@@ -0,0 +1,17 @@
+import React from 'react'
+const Gallery = () => {
+  return (
+    <div>
+      <h1 className="mt-5 text-center main-heading">Order your food</h1>
+      <hr />
+      <div className='menu-tabs container'>
+        <div className="menu-tab d-flex justify-content-center">
+            <button className="btn btn-warning">Breakfast</button>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Gallery
