33b511821ba93cfcf36fcb6d7794e9464eb01781
Lamia Fatiha <f6lamia@gmail.com>
2024-06-23 22:25:36 +0600

    food api file done

---
+++ src/Tabmenu/Menu.js
@@ -0,0 +1,69 @@
+const Menu = [
+   {
+    id: 1,
+    image: "img/1.jpeg",
+    name: "maggi",
+    category: "breakfast",
+    price: "TK 20",
+    description: "this is very tasty food."
+   },
+   {
+    id: 2,
+    image: "img/2.jpeg",
+    name: "maggi",
+    category: "breakfast",
+    price: "TK 20",
+    description: "this is very tasty food."
+   },
+   {
+    id: 3,
+    image: "img/3.jpeg",
+    name: "maggi",
+    category: "breakfast",
+    price: "TK 20",
+    description: "this is very tasty food."
+   },
+   {
+    id: 4,
+    image: "img/4.jpeg",
+    name: "maggi",
+    category: "breakfast",
+    price: "TK 20",
+    description: "this is very tasty food."
+   },
+   {
+    id: 5,
+    image: "img/5.jpeg",
+    name: "maggi",
+    category: "breakfast",
+    price: "TK 20",
+    description: "this is very tasty food."
+   },
+   {
+    id: 6,
+    image: "6",
+    name: "maggi",
+    category: "breakfast",
+    price: "TK 20",
+    description: "this is very tasty food."
+   },
+   {
+    id: 7,
+    image: "img/1.jpeg",
+    name: "maggi",
+    category: "breakfast",
+    price: "TK 20",
+    description: "this is very tasty food."
+   },
+   {
+    id: 8,
+    image: "img/5.jpeg",
+    name: "maggi",
+    category: "breakfast",
+    price: "TK 20",
+    description: "this is very tasty food."
+   }
+]
+
+
+export default Menu;
