ce5e3fccd45a0e334e08bde66d4d5817688d1e19
Lamia Fatiha <f6lamia@gmail.com>
2024-06-25 13:28:54 +0600

    card done

--- src/Tabmenu/Gallery.js
+++ src/Tabmenu/Gallery.js
@@ -11,9 +11,39 @@ const Gallery = () => {
             <button className="btn btn-warning">Evening</button>
             <button className="btn btn-warning">Dinner</button>
             <button className="btn btn-warning">All</button>
-
         </div>
+      </div>
+      <div className='menu-item container-fluid mt-5'>
+          <div className="row">
+            <div className="col-11 mx-auto">
+              <div className="row my-5">
+              <div className="item col-12 col-md-6 col-lg-6 col-xl-4">
+                 <div className="row item-inside">
+                  <div className="col-12 col-md-12 col-lg-4 img-div">
+                    <img src="" alt="menuPic" className="img-fluid" />
+                  </div>
+                  <div className="col-12 col-md-12 col-lg-8">
+                    <div className="main-title pt-4 pb-3">
+                      <h1>Maggi</h1>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                    <div className="menu-price-book">
+                      <div className="price-book-divide">
+                        <h2>Price : TK120</h2>
+                        <a href="#">
+                          <button className='btn btn-primary'>Order now</button>
+                        </a>
+                      </div>
+                      <p>*Price may vary on selected date</p>
+                    </div>
+                  </div>
+                 </div>
 
+              </div>
+
+              </div>
+            </div>
+          </div>
       </div>
     </div>
   )
